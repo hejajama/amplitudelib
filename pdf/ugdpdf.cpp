7bb8fd81c8ebc79a93ef72a0846d3bef702a7155
Heikki Mäntysaari <heikki.mantysaari@jyu.fi>
2012-12-03 13:21:06 +0200

    New PDF: compute pdf from UGD

---
+++ pdf/ugdpdf.cpp
@@ -0,0 +1,71 @@
+/*
+ * PDF from UGD
+ * Computes UGD from dipole amplitude and integrates it over k_T
+ * to get PDF
+ * 
+ * Note: requires pointer to AmplitudeLib* and the scaling
+ * factor \sigma_0/2
+ *
+ * Heikki Mäntysaari <heikki.mantysaari@jyu.fi>, 2012
+ */
+
+#include "ugdpdf.hpp"
+#include <string>
+
+UGDPDF::UGDPDF()
+{
+	cerr << "UGDPDF requires AmplitdeLib*, panicking!" << endl;
+	N=NULL;
+	exit(1);
+}
+
+UGDPDF::UGDPDF(AmplitudeLib* N_, double sigma02_)
+{
+	N=N_; sigma02=sigma02_;
+}
+
+std::string UGDPDF::GetString()
+{
+	return "UGD PDF" ;
+}
+
+double UGDPDF::xq(double x, double q, Parton p)
+{
+	if (p != G)
+	{
+		//cerr << "UGDPDF can only compute gluon distribution! " << endl;
+		return 0;
+	}
+	
+	if (x < MinX())
+	{
+		cerr << "x < MinX()=" << MinX() << ", exit..." << endl;
+		exit(1);
+	}
+	if (x>MaxX())
+	{
+		//cerr << "x > MaxX()=" << MaxX()<< ", exiting.." << endl;
+		x = MaxX();
+		//return 0;
+		//exit(1);
+	}
+	
+	return sigma02*N->xg(x, q);
+}
+
+UGDPDF::~UGDPDF()
+{
+	if (N != NULL)
+		delete N;
+}
+
+double UGDPDF::MaxX()
+{
+	return N->X0();
+}
+
+double UGDPDF::MinX()
+{
+	return N->X0()*std::exp(-N->MaxY());
+}
+
---
+++ pdf/ugdpdf.hpp
@@ -0,0 +1,47 @@
+#ifndef _UGDPDF_H
+#define _UGDPDF_H
+
+/*
+ * PDF from UGD
+ * Computes UGD from dipole amplitude and integrates it over k_T
+ * to get PDF
+ * 
+ * Note: requires pointer to AmplitudeLib* and the scaling
+ * factor \sigma_0/2
+ * 
+ * NOTE: Deletes the AmplitudeLib object in destructor! So assumes
+ * that AmplitudeLib* N is not used outside this class!
+ *
+ * Heikki Mäntysaari <heikki.mantysaari@jyu.fi>, 2012
+ */
+
+#include "../tools/config.hpp"
+#include "pdf.hpp"
+#include "../amplitudelib/amplitudelib.hpp"
+#include <string>
+
+class UGDPDF : public PDF
+{
+    public:
+        double xq(double x, double q, Parton p);    // return x*q(x,q)
+        //void Initialize(int param=-1);
+        std::string GetString();
+        
+        double MaxX();
+        double MinX();
+        
+        UGDPDF();
+        UGDPDF(AmplitudeLib* N, double sigma02_=1.0);
+        ~UGDPDF();
+        
+        //void Test();
+    private:
+		AmplitudeLib* N;
+		double sigma02 ;
+
+
+};
+
+
+#endif
+
